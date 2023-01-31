b1cfafe9fe75a1c855fa6482fa165e16e1def660
lutfi-pratama <pratamalutfi60@gmail.com>
2023-01-31 22:53:56 +0700

    fix splash screen icon logo and menu icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,7 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter/material.dart';
-import '../resources/resources.dart';
-import './di/injections.dart';
-import './pages/dashboard/dashboard_page.dart';
-import 'core/load_data_repository_exe.dart';
-import 'cubits/load_data/load_data_cubit.dart';
-import 'cubits/menu_item/menu_item_cubit.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import './di/index.dart';
+import 'my_app.dart';
 
 void main() {
   final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
@@ -16,47 +10,4 @@ void main() {
 
   runApp(const MyApp());
   FlutterNativeSplash.remove();
-}
-
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  final LoadDataCubit _loadDataCubit =
-      LoadDataCubit(repository: LoadDataRepositoryExe());
-  final MenuItemCubit _menuItemCubit = MenuItemCubit();
-
-  @override
-  void dispose() {
-    _loadDataCubit.close();
-    _menuItemCubit.close();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return ScreenUtilInit(
-        designSize: const Size(360, 800),
-        builder: (_, __) {
-          return MultiBlocProvider(
-            providers: [
-              BlocProvider.value(
-                value: _loadDataCubit,
-              ),
-              BlocProvider.value(
-                value: _menuItemCubit,
-              )
-            ],
-            child: MaterialApp(
-              title: 'GitHub Search',
-              theme: ThemeData(fontFamily: StringConstant.poppins),
-              home: const Dashboard(),
-            ),
-          );
-        });
-  }
 }
